b1c64fcb2ffd9186428fd12a3cd6f446d40c5cd2
Ubaidkhan <ubaidkhan0631@gmail.com>
2022-01-08 18:08:26 +0530

    hide secret key

--- src/watchmate/settings.py
+++ src/watchmate/settings.py
@@ -10,19 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 import os
-import dotenv
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3nn#^xb-m38kl@^w+)py6m@7$1&h(lpqq9%lp+v#j$%6x3z=n&'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
